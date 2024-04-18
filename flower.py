34cd567452b21c6cc8037b78819c477541af711c
GunjanMh <167431048+GunjanMh@users.noreply.github.com>
2024-04-18 17:14:26 +0530

    Add files via upload

---
+++ flower.py
@@ -0,0 +1,15 @@
+#This is second of the 3 random madlib samples called flower, that we will ask the program to choose from.
+
+
+color= input("Color: ")
+feeling = input("Feeling: ")
+smell= input("Smell: ")
+shape= input("Shape: ")
+name= input("Name: ")
+
+
+madlibs= f"It was only {color} o'clocl when the bell rang. There was a feeling of {feeling}\
+and noone was at home. It was at {smell} when I was just {shape} years old. I was called {name}."
+
+print(madlibs)
+                 
